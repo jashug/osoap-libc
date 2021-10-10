10dc707341822a575bfdf4a6b9335f061d719612
Jasper Hugunin <jasper@hugunin.net>
2021-10-10 14:10:35 -0700

    Resize jmp_buf

--- arch/wasm32/bits/setjmp.h
+++ arch/wasm32/bits/setjmp.h
@@ -1,2 +1,5 @@
-// This is a stub for now
-typedef unsigned long __jmp_buf[4];
+// #ifdef OSOAP_USE_FORK
+typedef unsigned long __jmp_buf[4096 / sizeof(long) + 12];
+// #else
+// typedef unsigned long __jmp_buf[6];
+// #endif
